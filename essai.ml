c16125c362f9f5a2e12c49e7acf0a12a62994c01
filliatr <>
2001-06-13 13:06:49 +0000

    essai

---
+++ essai.ml
@@ -0,0 +1,34 @@
+(*
+ * ocamlweb - A WEB-like tool for ocaml
+ * Copyright (C) 1999 Jean-Christophe FILLIATRE
+ * 
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License version 2 for more details
+ * (enclosed in the file GPL).
+ *)
+
+(* $Id$ *)
+
+(* un commentaire "avec
+   une chaine sur deux \n ligne
+   ou sur trois" voila
+   et voila *)
+
+du code (* "\\\"" *)
+
+(* *) "du code" (* "et hop" *)
+
+(*
+
+un commentaire a la Claude
+
+*)
+
+
